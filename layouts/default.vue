7fd3be89a64859cc3479a48416d24c4cb579362f
domalasherpa <domalasherpa9813@gmail.com>
2024-09-15 17:43:56 +0545

    feat: add basic layout for speaker page

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Header />
+        <div class="w-full flex justify-center">
+            <div class="w-9/12 text-[#030328]">
+                <slot />
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
