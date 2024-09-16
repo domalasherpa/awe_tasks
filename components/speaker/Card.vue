ca506137f5753b32a00f7a9a2bc04ba05db1d350
domalasherpa <domalasherpa9813@gmail.com>
2024-09-16 10:23:53 +0545

    feat: create separate speaker folder

---
+++ components/speaker/Card.vue
@@ -0,0 +1,20 @@
+<script setup>
+  const props = defineProps({
+    speaker: Object
+  })
+</script>
+
+<template>
+    <div class="max-w-52 space-y-2 bg-white text-xs">
+        <div class="w-full md:h-28 lg:h-56 overflow-clip">
+            <img :src="speaker.profileImage" :alt="speaker.name" class="w-full object-contain object-center">
+        </div>
+        <div class="border-b-2 md:py-2 border-[#3384BC]">
+            <h1 class="font-semibold text-[#030328] sm:text-md lg:text-xl h-14">{{ speaker.name }}</h1>
+        </div>
+        <div class="">
+            <p class="text-[#252643]">{{ speaker.designation  }}</p>
+            <p class="font-semibold text-[#030328]">{{ speaker.companyName }}</p>
+        </div>
+    </div>
+</template>
