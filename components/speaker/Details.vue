e50549b49c480fdb35dd27372702b7d381a29c12
domalasherpa <domalasherpa9813@gmail.com>
2024-09-16 18:03:51 +0545

    feat: display speaker detail

--- components/speaker/Details.vue
+++ components/speaker/Details.vue
@@ -1,18 +1,19 @@
 <script setup>
     const route = useRoute()
-    const id = route.params.id
-    const apiUrl = `https://swa-2024-dev.up.railway.app/api/speakers/${id}`;
-    const {status, data} = await useFetch(apiUrl, {key:'singleSpeaker'}); 
+    const id = ref(route.params.id)
+
+    const apiUrl = `https://swa-2024-dev.up.railway.app/api/speakers/${id.value}`;
+    const {status, data} = await useFetch(apiUrl);
 </script>
 <template>
     <div v-if="status=='pending'">
         <h1>Loading...</h1>
     </div>
-    <div v-else class="w-full md:flex md:h-[500px] border-2 shadow-xl text-sm overflow-clip">
-        <div class="">
-            <img :src="data.speaker.profileImage" alt="profile image" class="w-full h-full object-contain object-center">
+    <div v-else class="lg:flex  border-2 shadow-xl text-sm overflow-clip">
+        <div class="lg:w-96">
+            <img :src="data.speaker.profileImage" alt="profile image" class="w-full h-full object-cover object-center">
         </div>
-        <div class="md:w-3/5 p-4 md:px-14 md:py-8 space-y-4">
+        <div class="lg:w-3/5 p-4 md:px-14 md:py-8 space-y-4">
             <div>
                 <h1 class="font-semibold text-2xl">{{ data.speaker.name }}</h1>
                 <h2 class="font-semibold">{{  data.speaker.designation }}</h2>
