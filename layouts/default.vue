1ca76c2b7a3414d8b8db392449e1e6cdf3de807b
domalasherpa <domalasherpa9813@gmail.com>
2024-09-17 17:47:45 +0545

    feat: adds custom header style for speaker page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Header />
+        <Header class="bg-gradient-to-r from-[#00012D] to-[#03025f]"/>
         <div class="w-full flex justify-center">
             <div class="w-9/12 text-[#030328]">
                 <slot />
