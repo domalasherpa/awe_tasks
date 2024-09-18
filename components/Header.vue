8e91c1242e225efdf8472c4a9a4a7b97707578b2
domalasherpa <domalasherpa9813@gmail.com>
2024-09-18 15:49:46 +0545

    "feat: adds slide over"

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-const props = defineProps({
-    classStyle: String
-})
-
 const links = [{
     label: 'Home',
     icon: '',
@@ -55,74 +51,74 @@ const items = [
     [{
         label: 'item3'
     }]
-
 ]
 
-const isOpen = ref(false);
-const hamIconName = computed(() => {
-    return isOpen.value === false ? 'ic:baseline-menu' : 'ic:outline-close';
-})
-const toggle = () => {
-    isOpen.value = !isOpen.value;
-}
-
+const isOpen = ref(false)
 </script>
 
 <template>
-    <div :class="classStyle"
-        class="relative bg-[url('/assets/img/speaker-header.png')] bg-[center_96rem] text-white font-semibold text-sm ">
-        <div class="absolute w-full h-full bg-gradient-to-b from-black to-transparent"></div>
-        <div class="py-8 px-20">
-            <div class="flex items-center justify-center md:justify-end space-x-4">
-                <div>
-                    <NuxtLink to="/register" class="flex items-center space-x-2">
-                        <span>Register now</span>
-                        <UIcon name="heroicons:arrow-long-right-20-solid" class="w-5 h-5" />
-                    </NuxtLink>
-                </div>
-                <div>
-                    <NuxtLink to="/login" class="flex items-center space-x-2">
-                        <span>Login</span>
-                        <UIcon name="material-symbols-light:lock-outline" class="w-5 h-5" />
-                    </NuxtLink>
-                </div>
-                <UDropdown :items="items" mode="hover" :popper="{ placement: 'bottom-start' }">
-                    <UButton label="Options" trailing-icon="i-heroicons-chevron-down-20-solid"
-                        class="font-bold bg-transparent hover:bg-transparent" />
-                </UDropdown>
+    <div class="bg-gradient-to-r from-[#00012D] to-[#03025f] text-white font-semibold py-4 px-2 md:px-20 space-y-4 text-sm">
+        <div class="flex items-center justify-end space-x-4 text-xs">
+            <div>
+                <NuxtLink to="/register" class="flex items-center md:space-x-2">
+                    <span >Register now</span>
+                    <UIcon name="heroicons:arrow-long-right-20-solid" class="w-5 h-5" />
+                </NuxtLink>
             </div>
-            <div class="flex justify-between">
-                <div>
-                    <img src="/assets/img/logo.png" alt="">
-                </div>
-                <!-- Change this in md -->
-                <div class="hidden lg:flex w-3/4 lg:justify-evenly lg:items-center">
-                    <div v-for="link in links">
-                        <NuxtLink :to="link.to" active-class="text-[#0058A0]">
-                            {{ link.label }}
-                        </NuxtLink>
-                    </div>
-                    <div class="bg-[#0058A0]  py-2 px-10 clip-para flex items-center space-x-2 cursor-pointer">
-                        <UIcon name="ic:baseline-video-camera-front" class="w-6 h-5" />
-                        <h1>Live Streaming</h1>
-                    </div>
-                </div>
-                <UIcon :name="hamIconName" class="lg:hidden w-10 h-10 cursor-pointer" @click="toggle" />
+            <div>
+                <NuxtLink to="/login" class="flex items-center md:space-x-2">
+                    <span>Login</span>
+                    <UIcon name="material-symbols-light:lock-outline" class="w-5 h-5" />
+                </NuxtLink>
             </div>
-            <div v-if="isOpen">
+            <UDropdown :items="items" mode="hover" :popper="{ placement: 'bottom-start' }">
+                <UButton label="Options" trailing-icon="i-heroicons-chevron-down-20-solid"
+                    class="font-bold bg-transparent hover:bg-transparent" />
+            </UDropdown>
+        </div>
+        <div class="flex justify-between">
+            <div>
+                <img src="/assets/img/logo.png" alt="">
+            </div>
+            <!-- Change this in md -->
+            <div class="hidden lg:flex w-3/4 lg:justify-evenly lg:items-center">
                 <div v-for="link in links">
                     <NuxtLink :to="link.to" active-class="text-[#0058A0]">
                         {{ link.label }}
                     </NuxtLink>
                 </div>
-                <div class="bg-[#0058A0]  py-2 px-6 clip-para flex w-fit cursor-pointer">
+                <div class="bg-[#0058A0]  py-2 px-10 clip-para flex items-center space-x-2 cursor-pointer">
                     <UIcon name="ic:baseline-video-camera-front" class="w-6 h-5" />
                     <h1>Live Streaming</h1>
                 </div>
             </div>
-            <slot />
+            <UIcon name='ic:baseline-menu' class="lg:hidden w-10 h-10 cursor-pointer" @click="isOpen = true" />
         </div>
-        
+        <div class="md:hidden">
+            <USlideover v-model="isOpen" prevent-close>
+                <UCard class="flex flex-col flex-1"
+                    :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+                    <template #header>
+                        <div class="flex items-center justify-end">
+                            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+                                @click="isOpen = false" />
+                        </div>
+                    </template>
+
+                    <Placeholder class="h-full space-y-2">
+                        <div v-for="link in links">
+                            <NuxtLink :to="link.to" active-class="text-[#0058A0]">
+                                {{ link.label }}
+                            </NuxtLink>
+                        </div>
+                        <div class="bg-[#0058A0]  py-2 px-6 clip-para flex w-fit cursor-pointer">
+                            <UIcon name="ic:baseline-video-camera-front" class="w-6 h-5" />
+                            <h1>Live Streaming</h1>
+                        </div>
+                    </Placeholder>
+                </UCard>
+            </USlideover>
+        </div>
     </div>
 </template>
 
