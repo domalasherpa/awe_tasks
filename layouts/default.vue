aa8a60edab2aff77697a9bde431380ab9c458e6d
domalasherpa <domalasherpa9813@gmail.com>
2024-09-18 15:40:01 +0545

    feat: increase div width for small devices

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <Header class="bg-gradient-to-r from-[#00012D] to-[#03025f]"/>
+        <Header class="bg-gradient-to-r from-[rgb(0,1,45)] to-[#03025f]"/>
         <div class="w-full flex justify-center">
-            <div class="w-9/12 text-[#030328]">
+            <div class="w-10/12 md:w-9/12 text-[#030328]">
                 <slot />
             </div>
         </div>
