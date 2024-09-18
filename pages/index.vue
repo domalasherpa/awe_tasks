16f7d4c412faa1a3d592ad4576be22df5ce865bf
domalasherpa <domalasherpa9813@gmail.com>
2024-09-18 17:33:26 +0545

    feat: decrease gird cols size and padding for small screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,22 @@
 <script setup>
-    const apiUrl = 'https://swa-2024-dev.up.railway.app/api/speakers'
-    const {status, data: speakers} = await useFetch(apiUrl)
+const speakerApiUrl = 'https://swa-2024-dev.up.railway.app/api/speakers';
+const { data:speakers, error, status } = await useFetch(speakerApiUrl);
 </script>
+
 <template>
-    <div class="w-full space-y-10 md:my-20">
-        <div>
-            <h1 class="text-4xl font-semibold">Speakers</h1>
-        </div>
-        <div v-if="status === 'pending'">
-            <h1>Loading...</h1>
-        </div>
-        <div v-else class="grid grid-cols-5 gap-x-2 gap-y-6">
-            <div v-for="speaker in speakers">
-                <SpeakerCard :speaker="speaker"/>
+    <NuxtLayout>
+        <div class="w-full space-y-10 my-10 md:my-20">
+            <div>
+                <h1 class="text-4xl font-semibold">Speakers</h1>
+            </div>
+            <div v-if="status === 'pending'">
+                <h1>Loading...</h1>
+            </div>
+            <div v-else class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-x-2 gap-y-6">
+                <div v-for="speaker in speakers">
+                    <SpeakerCard :speaker="speaker" />
+                </div>
             </div>
         </div>
-    </div>
+    </NuxtLayout>
 </template>
