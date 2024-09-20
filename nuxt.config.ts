0a98d02aad21a92aabe4397f978e9dd2e8633ff1
domalasherpa <domalasherpa9813@gmail.com>
2024-09-20 12:54:19 +0545

    feat: add custom tailwind template rows

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  tailwindcss: {
+    config:{
+      theme: {
+        extend: {
+          gridTemplateColumns: {
+            'custom': '1fr 2fr 1fr',
+          },
+          gridTemplateRows: {
+            'custom': 'auto 1fr 1fr',
+          },
+        },
+      },
+    }
+  }
 })
