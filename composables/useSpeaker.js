68f19635b98204e2b66ef47790c7947ed9cf0b86
domalasherpa <domalasherpa9813@gmail.com>
2024-09-20 10:15:58 +0545

    feat: create speaker composable for fetching data

---
+++ composables/useSpeaker.js
@@ -0,0 +1,19 @@
+const speakerApiUrl = 'https://swa-2024-dev.up.railway.app/api/speakers';
+
+export default function useSpeaker() {
+    const getSpeakers = async () => {
+        const { data, error, status } = await useFetch(speakerApiUrl);
+        return { data, error, status };
+    }
+
+    const getSingleSpeaker = async (id) => {
+        const speaker = `${speakerApiUrl}/${id.value}`;
+        const { data, error, status } = await useFetch(speaker);
+        return { data, error, status };
+    }
+
+    return {
+        getSpeakers,
+        getSingleSpeaker
+    }
+}
