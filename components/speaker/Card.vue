0b13daf11d7bc64c7dfa62c950df532582e199fe
domalasherpa <domalasherpa9813@gmail.com>
2024-09-20 12:53:34 +0545

    feat: add grid row 3 for each card

--- components/speaker/Card.vue
+++ components/speaker/Card.vue
@@ -7,12 +7,12 @@
 
 <template>
     <NuxtLink :to="{name: 'speaker-id', params: {id:speaker.id}}">
-        <div class="space-y-2 bg-white text-xs">
-            <div class="border-2  h-40 md:h-60 overflow-clip">
+        <div class="bg-white text-xs grid grid-rows-custom h-full">
+            <div class="border-2  h-60 overflow-clip">
                 <img :src="useApiUrl(speaker.profileImage)" :alt="speaker.name" class="w-full h-full object-cover object-center">
             </div>
             <div class="border-b-2 md:py-2 border-[#3384BC]">
-                <h1 class="font-semibold text-[#030328] sm:text-md lg:text-xl h-14">{{ speaker.name }}</h1>
+                <h1 class="font-semibold text-[#030328] sm:text-md lg:text-xl">{{ speaker.name }}</h1>
             </div>
             <div class="">
                 <p class="text-[#252643]">{{ speaker.designation  }}</p>
